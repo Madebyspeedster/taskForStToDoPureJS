b729baac19658361532a507a8723abf60ac8c139
madebyspeedster <madebyspeedster@gmail.com>
2019-05-12 18:51:37 +0300

    added folder functionality

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ application.elements = {};
 application.elements.buttons = {};
 application.elements.buttons.addTask = document.querySelector("#addTask");
 application.elements.buttons.highPr = document.querySelector("#highPr");
+application.elements.buttons.closeTodo = document.querySelector(".close_todo");
 application.elements.inputs = {};
 application.elements.inputs.taskInput = document.querySelector(".todo_input");
 application.elements.inputs.checkBoxInput = document.querySelector(".todo_input_checkbox");
 application.elements.list = document.querySelector("ul");
+application.elements.mainContainer = document.querySelector(".todo_container");
+application.elements.img = {};
+application.elements.img.todoIcon = document.querySelector(".folder_icon");
+application.elements.img.folderBlock = document.querySelector(".folder_block");
 application.utils = {};
 application.utils.methods = {};
 application.utils.methods.removeTask = function (element) {
@@ -39,4 +44,12 @@ application.elements.inputs.taskInput.addEventListener("keypress", (event) => {
             event.target.value = "";
         }
     }
+});
+application.elements.img.todoIcon.addEventListener("dblclick", () => {
+    application.elements.img.folderBlock.style.opacity = "0";
+    application.elements.mainContainer.classList.toggle("openTodoContainer");
+});
+application.elements.buttons.closeTodo.addEventListener("click", () => {
+    application.elements.mainContainer.classList.toggle("openTodoContainer");
+    application.elements.img.folderBlock.style.opacity = "1";
 });
